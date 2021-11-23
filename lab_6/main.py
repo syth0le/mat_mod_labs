1e18b406bd53b7f36fccc2be5ba77e8d27f42763
syth0le <chdan565@gmail.com>
2021-11-24 00:01:12 +0300

    add table data representation

--- lab_6/main.py
+++ lab_6/main.py
@@ -3,9 +3,9 @@ import numpy as np
 from draw_graphic import drawGraphic
 from fibonacci import get_extremum_of_function_by_fibonacci_method
 from golden_section import golden_section
+from table import print_table
 
 func = lambda x: x ** 2 + 2 * np.exp(x)
-
 # так как у функция возврастающая на положительных иксах и убывающая на отрицательных
 # проще найти минимумы
 
@@ -16,9 +16,10 @@ if __name__ == '__main__':
     eps = float(input('Enter accuracy: '))
     qa = input('Do u want to set N - number of iterations for fibonacci method [y/n]: ')
     if qa == 'y':
-        n = input('Enter number of iterations for fibonacci method: ')
+        n = int(input('Enter number of iterations for fibonacci method: '))
     else:
         n = None
     drawGraphic(a, b, func)
-    print(golden_section(func, a, b, eps))
-    print(get_extremum_of_function_by_fibonacci_method(func, a, b, eps, n))
+    gss = golden_section(func, a, b, eps)
+    fib = get_extremum_of_function_by_fibonacci_method(func, a, b, eps, n)
+    print_table(gss, fib)
---
+++ lab_6/table.py
@@ -0,0 +1,15 @@
+from rich.console import Console
+from rich.table import Table
+
+
+def print_table(gss: tuple, fib: tuple) -> None:
+    console = Console()
+    table = Table(title='Find Function Extremum')
+    table.add_column('Method name', justify='right', style='cyan', no_wrap=True)
+    table.add_column('function in Xm value', style='magenta')
+    table.add_column('Xm dot value', style='red on white')
+    table.add_column('Counted iterations number', style='green')
+
+    table.add_row('Golden-Section Method', *list(map(str, gss)))
+    table.add_row('Fibonacci Method', *list(map(str, fib)))
+    console.print(table)
