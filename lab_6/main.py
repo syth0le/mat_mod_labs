15c6c7a48ddeae7e0ce8c51a1f892daf801eb6c3
syth0le <chdan565@gmail.com>
2021-11-23 22:54:46 +0300

    add golden-section method

--- lab_6/main.py
+++ lab_6/main.py
@@ -1,12 +1,15 @@
 from typing import Callable
 
 import numpy as np
-from matplotlib import pyplot as plt
 import seaborn as sns
+from matplotlib import pyplot as plt
+
 # https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D1%82%D0%BE%D0%B4_%D0%B7%D0%BE%D0%BB%D0%BE%D1%82%D0%BE%D0%B3%D0%BE_%D1%81%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D1%8F
 
 
 func = lambda x: x ** 2 + 2 * np.exp(x)
+
+
 # так как у функция возврастающая на положительных иксах и убывающая на отрицательных
 # проще найти минимумы
 
@@ -18,26 +21,28 @@ def drawGraphic(a, b, func):
     plt.show()
 
 
-def golden_section(a: int, b: int, eps: float, func: Callable):
-    fi = 0.5 * (1 + np.sqrt(5))  # пропорция золотого сечения
+def golden_section(func: Callable, a: int, b: int, eps=1e-5):
+    fi = (1 + np.sqrt(5)) / 2  # пропорция золотого сечения
     x1 = b - (b - a) / fi
     x2 = a + (b - a) / fi
-    y1 = func(x1)
-    y2 = func(x2)
-    x = 0
-    while np.abs(b - a) < eps:
-        if y1 > y2:
-            a = x1
-        else:
+
+    while abs(b - a) > eps:
+        if func(x1) < func(x2):
             b = x2
+        else:
+            a = x1
+
+        x1 = b - (b - a) / fi
+        x2 = a + (b - a) / fi
 
     x = (a + b) / 2
-    return x, func(x), f'Iterations: {iterations}'
+    return x, func(x), \
+        # f'Iterations: {iterations}'
 
 
-def fibonacci_method(a: int, b: int, n: int, func: Callable):
-    x1 = a + (b - a) * fibonacci_method(a, b, n-2, func) / fibonacci_method(a, b, n, func)
-    x2 = a + (b - a) * fibonacci_method(a, b, n-1, func) / fibonacci_method(a, b, n, func)
+def fibonacci_method(func: Callable, a: int, b: int, n: int):
+    x1 = a + (b - a) * fibonacci_method(a, b, n - 2, func) / fibonacci_method(a, b, n, func)
+    x2 = a + (b - a) * fibonacci_method(a, b, n - 1, func) / fibonacci_method(a, b, n, func)
     y1 = func(x1)
     y2 = func(x2)
     while n > 1:
@@ -56,7 +61,8 @@ def fibonacci_method(a: int, b: int, n: int, func: Callable):
             y1 = func(x1)
 
     x = (x1 + x2) / 2
-    return x, func(x), f'Iterations: {iterations}'
+    return x, func(x), \
+        # f'Iterations: {iterations}'
 
 
 if __name__ == '__main__':
@@ -70,8 +76,10 @@ if __name__ == '__main__':
     # drawGraphic(a, 0, func)
     # drawGraphic(0, b, func)
     drawGraphic(a, b, func)
-    print(golden_section(a, b, eps, func))
-    print(fibonacci_method(a, b, 10, func))
+    print(golden_section(func, a, b, eps))
+    print(golden_section(func, a, b))
+
+    print(fibonacci_method(func, a, b, 10))
     # for i in range(15):
     #     temp = float(f'-{i}')
     #     print((i, func(i)), (temp, func(temp)))
