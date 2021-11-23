2d00109fbcb4f8fee1596c15624cc5f2150a8bee
syth0le <chdan565@gmail.com>
2021-11-23 23:41:01 +0300

    update inputs in main file

--- lab_6/main.py
+++ lab_6/main.py
@@ -14,7 +14,11 @@ if __name__ == '__main__':
     a = int(input('Enter left endside: '))
     b = int(input('Enter right endside: '))
     eps = float(input('Enter accuracy: '))
-    n = input('Enter number of iterations for fibonacci method: ')
+    qa = input('Do u want to set N - number of iterations for fibonacci method [y/n]: ')
+    if qa == 'y':
+        n = input('Enter number of iterations for fibonacci method: ')
+    else:
+        n = None
     drawGraphic(a, b, func)
     print(golden_section(func, a, b, eps))
-    print(get_extremum_of_function_by_fibonacci_method(func, a, b, eps))
+    print(get_extremum_of_function_by_fibonacci_method(func, a, b, eps, n))
